15ee16ad8506fb558a34fe6560a84b46f5f90048
Baker.He <Baker.He@zoom.us>
2019-01-18 15:16:25 +0800

    shuffle

--- src/common/util.js
+++ src/common/util.js
@@ -45,5 +45,15 @@ module.exports = {
     outputParts = outputParts.concat(toParts.slice(samePartsLength));
 
     return outputParts.join('/');
+  },
+  shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
   }
-}
+};
