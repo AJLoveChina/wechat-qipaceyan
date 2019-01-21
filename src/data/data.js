59ec8ec71e1d948126f7234b5b2209b775f9e8a2
Baker.He <Baker.He@zoom.us>
2019-01-21 16:25:25 +0800

    hefei

--- src/data/data.js
+++ src/data/data.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const yxlm = require('./yxlm');
 const zjl = require('./zjl');
+const hefei = require('./hefei');
 let data = {
   yxlm,
   zjl,
+  hefei,
 };
 
 fs.writeFileSync(path.resolve("./dist-data/data.json"), JSON.stringify(data));
